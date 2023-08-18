5e147323db9a89e022f4ea49458933eafb1f6e4d
alpaypasali <alpaypasali@gmail.com>
2023-08-18 04:30:05 +0300

    Add project files.

---
+++ Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext:DbContext
+    {
+
+        protected IConfiguration Configuration { get; set; }
+        public DbSet<Brand> Brands { get; set; }  
+        
+        public BaseDbContext(DbContextOptions dbContextOptions , IConfiguration configuration) : base(dbContextOptions)
+        {
+
+            Configuration = configuration;
+            Database.EnsureCreated();   
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //mevcut assemblydeki configurationları bul ve uygula
+          modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); 
+
+        }
+
+    }
+}
